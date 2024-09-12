17e6b8a262c8e64f61e2d0c690d97ada9b5b743b
KrumBezmenov <krum.bezmenov@gmail.com>
2024-09-12 15:57:48 +0200

    Search functionality

--- src/js/model.js
+++ src/js/model.js
@@ -3,6 +3,10 @@ import { getJSON } from './helpers';
 
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
 };
 
 export const loadRecipe = async function (id) {
@@ -21,6 +25,26 @@ export const loadRecipe = async function (id) {
       ingredients: recipe.ingredients,
     };
     console.log(state.recipe);
+  } catch (error) {
+    console.error(`${error}`);
+    throw error;
+  }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}/?search=${query}`);
+    console.log(data);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
   } catch (error) {
     console.error(`${error}`);
     throw error;
