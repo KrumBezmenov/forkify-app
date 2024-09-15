328bc782389cf7ddc0e67164de4fcc3e0cb8b634
KrumBezmenov <krum.bezmenov@gmail.com>
2024-09-15 17:48:15 +0200

    pagination fix page reset

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -2565,6 +2565,7 @@ const loadSearchResults = async function(query) {
                 image: rec.image_url
             };
         });
+        state.search.page = 1;
     } catch (error) {
         console.error(`${error}`);
         throw error;
